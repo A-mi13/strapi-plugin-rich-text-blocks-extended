a020a9001ef30be2e8e660e1994efd1c119ae258
Alex Alex <alex-c13@mail.ru>
2025-07-31 12:05:24 +0300

    Fix Code.tsx: restore original working version with codeBlocks export

--- admin/src/utils/getTranslation.ts
+++ admin/src/utils/getTranslation.ts
@@ -1,5 +1,5 @@
-import { PLUGIN_ID } from '../pluginId';
+﻿import { PLUGIN_ID } from "../pluginId";
 
 const getTranslation = (id: string) => `${PLUGIN_ID}.${id}`;
 
-export { getTranslation };
+export { getTranslation, getTranslation as getTrad };
